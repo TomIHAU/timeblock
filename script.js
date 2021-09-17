c7ad594ebbc4f7cdc26ac58ef4c57e6848d11d47
TauH <thaywood988@gmail.com>
2021-09-17 17:40:04 +1000

    each loop working through the issues

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 $("#currentDay").text(moment().format("LLLL"));
+var clickBtn = $(".saveBtn");
 var LSarray = [];
+var currentHour = moment().format("hA")
+
+$("table tr td:first-child").each(function(){
+    console.log($(this).text())
+    // if(moment($(this.text()).format("hA").isAfter(currentHour)){
+    //     this.setAttribute("style", "background-color: green")
+    // }
+})
+
+//moment(variable input from table).format(hA) -- for the for/ if loop regarding background.
+
+
+
+
+
+
+
+function brTest(event){
+    event.stopPropagation();
+    event.clickBtn.setAttribute(
+        "style",
+        "background-color: #000"
+    )
+}
+
+clickBtn.addEventListener("click", brTest);
