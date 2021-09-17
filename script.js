3e1f11bb7fdae2ce00693f0837e2bee10e375dc6
TauH <thaywood988@gmail.com>
2021-09-17 19:27:14 +1000

    got the time dependent backgrounds to work

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 $("#currentDay").text(moment().format("LLLL"));
+
 var clickBtn = $(".clickMe");
 var LSarray = [];
 var currentHour = moment().format("ha")
-
 $("table tr td:first-child").each(function(){
-    if(moment($(this.text()))==moment().format("ha")){
+    if(moment($(this).text(), 'ha').isSame(moment(), "hour")){
         this.nextElementSibling.firstChild.setAttribute("style", "background-color: red")
-    } else if (moment($(this).text(), "ha").isBefore(moment())) {
+    } else if (moment($(this).text(), "ha").isAfter(moment())) {
    this.nextElementSibling.firstChild.setAttribute("style", "background-color: green")
-}});
+}
+});
 
 
 
