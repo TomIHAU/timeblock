6db898bc435304607a41f60dcdf2a83475ca1a3b
TauH <thaywood988@gmail.com>
2021-09-18 12:53:43 +1000

    worked out jquery issues with [] need to be .eq()

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
+//sets the clock on page load
 $("#currentDay").text(moment().format("LLLL"));
 
 var clickBtn = $(".clickMe");
-var LSarray = [];
-var currentHour = moment().format("ha")
-$("table tr td:first-child").each(function(){
+var LSarray =["hello","hello","hello","hello","hello","hello","hello","hello","hello","hello"];
+//LSarray = JSON.parse(localStorage.getItem("ToDos"));
+
+// function init(){
+//   JSON.parse(localStorage.getItem("ToDos"));
+//   localStorage.setItem("ToDos", JSON.stringify(LSarray));
+//   $("table tr td:first-child").each(function(){
+
+//   })
+// }
+
+// ------- ask about using i in an each loop
+var secondChild = $("textarea")
+
+for(let i = 0; i < secondChild.length; i++){
+  console.log(LSarray[i])
+  console.log(secondChild[i])
+  if(LSarray[i] === !null){
+   secondChild.eq(i).val(LSarray[i]);
+  
+}
+
+// $("table tr td")[1].each(function(){
+//   if Ls
+// })
+
+
+//colours the time zones
+$("tr td:first-child").each(function(){
     if(moment($(this).text(), 'ha').isSame(moment(), "hour")){
         this.nextElementSibling.firstChild.setAttribute("style", "background-color: red")
     } else if (moment($(this).text(), "ha").isAfter(moment())) {
-   this.nextElementSibling.firstChild.setAttribute("style", "background-color: green")
+        this.nextElementSibling.firstChild.setAttribute("style", "background-color: green")
 }
 });
+// function onClick(event){
+//     var element = this.target;
 
-
-
-
-
-
-
-function brTest(event){
-    event.stopPropagation();
-    event.clickBtn.setAttribute(
-        "style",
-        "background-color: #000"
-    )
-}
-
-clickBtn.addEventListener("click", brTest);
+// }
+//init();
+//clickBtn.addEventListener("click", function(event){});
