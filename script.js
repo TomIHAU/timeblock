3c6d95b0bd593775fe69efc90a78ae3e1e54c821
TauH <thaywood988@gmail.com>
2021-09-21 22:06:31 +1000

    updated css, minor js changes

--- script.js
+++ script.js
@@ -9,17 +9,16 @@ if (temp != null){
     LSarray = temp;
 }
 
-//------- ask about using i in an each loop
-//secondChild.each(function(){
-//  XXXXXXXXXXXXX
-//})
-
 for(let i = 0; i < secondChild.length; i++){
     secondChild[i].setAttribute("data-index", i);
     secondChild.eq(i).val(LSarray[i]);
 };
 
-//colours the time zones
+// $(secondChild).each(function(element, i){
+//     element.setAttribute("data-index", i);
+//     element.val(LSarray[i]);
+// })
+
 $("tr td:first-child").each(function(){
     if(moment($(this).text(), 'ha').isSame(moment(), "hour")){
         this.nextElementSibling.firstChild.setAttribute("style", "background-color: red")
@@ -27,6 +26,7 @@ $("tr td:first-child").each(function(){
         this.nextElementSibling.firstChild.setAttribute("style", "background-color: green")
 }
 });
+
 tableBtn.click(function(event){ 
     var element = event.target;
     console.log(element)
